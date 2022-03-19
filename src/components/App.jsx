c4f75ae3c912b9e2099ebec128c6eddd6b28c7fd
Joaquin Rosendo <joaquinrosendo@live.com>
2022-03-18 22:17:41 -0600

    add App and Seat components

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Seat from "./Seat";
+
+function App() {
+    return (
+        <div>
+            <h1>Movie Ticket App</h1>
+            <Seat />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Seat.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Seat() {
+    return (
+        <input type="checkbox" id="seat" name="seat" />
+    );
+}
+
+export default Seat;
